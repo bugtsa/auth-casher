7e450bb1c1f6392821106ec39ca7d3b236a809b8
Vladimir Polkhovtsev <bugtsa@gmail.com>
2020-03-23 09:28:12 +0300

    fix: add bad credential error message
    Signed-off-by: Vladimir Polkhovtsev <v.polovtsev@dlg.im>

--- src/main/kotlin/com/bugtsa/auth/casherauthserver/service/CustomUserDetailsService.kt
+++ src/main/kotlin/com/bugtsa/auth/casherauthserver/service/CustomUserDetailsService.kt
@@ -2,9 +2,11 @@ package com.bugtsa.auth.casherauthserver.service
 
 import com.bugtsa.auth.casherauthserver.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.security.authentication.AccountStatusUserDetailsChecker
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -16,14 +18,17 @@ class CustomUserDetailsService : UserDetailsService {
     private lateinit var userRepository: UserRepository
 
     override fun loadUserByUsername(input: String): UserDetails {
-        val user = if (isValidEmail(input)) {
-            userRepository.findByEmail(input)
-        } else {
-            userRepository.findByUsername(input)
+        return try {
+            val user = if (isValidEmail(input)) {
+                userRepository.findByEmail(input)
+            } else {
+                userRepository.findByUsername(input)
+            }
+            AccountStatusUserDetailsChecker().check(user)
+            user
+        } catch (e: EmptyResultDataAccessException) {
+            throw UsernameNotFoundException(input)
         }
-        AccountStatusUserDetailsChecker().check(user)
-
-        return user
     }
 
     private fun isValidEmail(email: String?): Boolean {
